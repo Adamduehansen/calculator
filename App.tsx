f29acd617425a833e05115d001890abfc559b885
Adam Due Hansen <adamduehan@gmail.com>
2020-12-19 23:39:33 +0100

    Adds number pad buttons.

--- App.tsx
+++ App.tsx
@@ -1,21 +1,68 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {
+  Button,
+  SafeAreaView,
+  StyleSheet,
+  TextInput,
+  View,
+} from 'react-native';
+import useCalculator from './src/useCalculator';
 
-export default function App() {
+const App = function () {
+  const { numberFieldValue, clear, addToNumberField } = useCalculator();
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View>
+      <StatusBar style='auto' />
+      <SafeAreaView>
+        <TextInput style={styles.numberField} value={numberFieldValue} />
+        <View style={styles.numberPadRow}>
+          <Button title='7' onPress={() => addToNumberField(7)} />
+          <Button title='8' onPress={() => addToNumberField(8)} />
+          <Button title='9' onPress={() => addToNumberField(9)} />
+          <Button title='÷' onPress={() => {}} />
+        </View>
+        <View style={styles.numberPadRow}>
+          <Button title='4' onPress={() => addToNumberField(4)} />
+          <Button title='5' onPress={() => addToNumberField(5)} />
+          <Button title='6' onPress={() => addToNumberField(6)} />
+          <Button title='✕' onPress={() => {}} />
+        </View>
+        <View style={styles.numberPadRow}>
+          <Button title='1' onPress={() => addToNumberField(1)} />
+          <Button title='2' onPress={() => addToNumberField(2)} />
+          <Button title='3' onPress={() => addToNumberField(3)} />
+          <Button title='-' onPress={() => {}} />
+        </View>
+        <View style={styles.numberPadRow}>
+          <Button title='0' onPress={() => addToNumberField(0)} />
+          <Button title=',' onPress={() => {}} />
+          <Button title='=' onPress={() => {}} />
+          <Button title='+' onPress={() => {}} />
+        </View>
+        <View style={styles.numberPadRow}>
+          <Button
+            title='Clear'
+            onPress={() => {
+              clear();
+            }}
+          />
+        </View>
+      </SafeAreaView>
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  numberField: {
+    textAlign: 'right',
+  },
+  numberPadRow: {
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'space-around',
   },
 });
+
+export default App;
