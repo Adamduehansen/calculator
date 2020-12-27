79c8ff9debffc12b9bfc21b036efd2d3740bb55d
Adam Due Hansen <adamduehan@gmail.com>
2020-12-27 11:39:30 +0100

    Adds useCalculator functions to all buttons.

--- App.tsx
+++ App.tsx
@@ -3,14 +3,20 @@ import React from 'react';
 import {
   Button,
   SafeAreaView,
-  StyleSheet,
   TextInput,
   View,
+  StyleSheet,
 } from 'react-native';
 import useCalculator from './src/useCalculator';
 
 const App = function () {
-  const { numberFieldValue, clear, addToNumberField } = useCalculator();
+  const {
+    numberFieldValue,
+    clear,
+    addToNumberField,
+    changeOperator,
+    calculate,
+  } = useCalculator();
 
   return (
     <View>
@@ -18,28 +24,103 @@ const App = function () {
       <SafeAreaView>
         <TextInput style={styles.numberField} value={numberFieldValue} />
         <View style={styles.numberPadRow}>
-          <Button title='7' onPress={() => addToNumberField(7)} />
-          <Button title='8' onPress={() => addToNumberField(8)} />
-          <Button title='9' onPress={() => addToNumberField(9)} />
-          <Button title='÷' onPress={() => {}} />
+          <Button
+            title='7'
+            onPress={() => {
+              addToNumberField(7);
+            }}
+          />
+          <Button
+            title='8'
+            onPress={() => {
+              addToNumberField(8);
+            }}
+          />
+          <Button
+            title='9'
+            onPress={() => {
+              addToNumberField(9);
+            }}
+          />
+          <Button
+            title='÷'
+            onPress={() => {
+              changeOperator('Divide');
+            }}
+          />
         </View>
         <View style={styles.numberPadRow}>
-          <Button title='4' onPress={() => addToNumberField(4)} />
-          <Button title='5' onPress={() => addToNumberField(5)} />
-          <Button title='6' onPress={() => addToNumberField(6)} />
-          <Button title='✕' onPress={() => {}} />
+          <Button
+            title='4'
+            onPress={() => {
+              addToNumberField(4);
+            }}
+          />
+          <Button
+            title='5'
+            onPress={() => {
+              addToNumberField(5);
+            }}
+          />
+          <Button
+            title='6'
+            onPress={() => {
+              addToNumberField(6);
+            }}
+          />
+          <Button
+            title='✕'
+            onPress={() => {
+              changeOperator('Times');
+            }}
+          />
         </View>
         <View style={styles.numberPadRow}>
-          <Button title='1' onPress={() => addToNumberField(1)} />
-          <Button title='2' onPress={() => addToNumberField(2)} />
-          <Button title='3' onPress={() => addToNumberField(3)} />
-          <Button title='-' onPress={() => {}} />
+          <Button
+            title='1'
+            onPress={() => {
+              addToNumberField(1);
+            }}
+          />
+          <Button
+            title='2'
+            onPress={() => {
+              addToNumberField(2);
+            }}
+          />
+          <Button
+            title='3'
+            onPress={() => {
+              addToNumberField(3);
+            }}
+          />
+          <Button
+            title='-'
+            onPress={() => {
+              changeOperator('Subtract');
+            }}
+          />
         </View>
         <View style={styles.numberPadRow}>
-          <Button title='0' onPress={() => addToNumberField(0)} />
-          <Button title=',' onPress={() => {}} />
-          <Button title='=' onPress={() => {}} />
-          <Button title='+' onPress={() => {}} />
+          <Button
+            title='0'
+            onPress={() => {
+              addToNumberField(0);
+            }}
+          />
+          <Button title=',' disabled={true} onPress={() => {}} />
+          <Button
+            title='='
+            onPress={() => {
+              calculate();
+            }}
+          />
+          <Button
+            title='+'
+            onPress={() => {
+              changeOperator('Add');
+            }}
+          />
         </View>
         <View style={styles.numberPadRow}>
           <Button
